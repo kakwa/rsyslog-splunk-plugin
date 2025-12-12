c57b6ec36b065d7cadb9cfd15323fb698ec2522f
kakwa <carpentier.pf@gmail.com>
2025-12-12 14:27:10 +0100

    rough initial conversion of https://github.com/mikedickey/go-s2s into a C rsyslog plugin

---
+++ config.h
@@ -0,0 +1,13 @@
+/* config.h - Configuration header for omsplunks2s rsyslog module */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Module version */
+#define MODULE_VERSION "1.0.0"
+
+/* Feature flags */
+#define HAVE_PTHREAD 1
+
+#endif /* CONFIG_H */
+
